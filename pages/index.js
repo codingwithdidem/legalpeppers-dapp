c626644e228ee265058a3c2b312163738f162630
codingwithdidem <codingwithdidem@gmail.com>
2022-03-28 22:54:16 +0300

    new design

--- pages/index.js
+++ pages/index.js
@@ -88,13 +88,8 @@ export default function Mint() {
   };
 
   return (
-    <div className="min-h-screen h-full w-full overflow-hidden flex flex-col items-center justify-center bg-brand-background ">
+    <div className="min-h-screen h-full w-full overflow-hidden flex flex-col items-center justify-center">
       <div className="relative w-full h-full flex flex-col items-center justify-center">
-        <img
-          src="/images/meshPinky.jpg"
-          className="absolute inset-auto block w-full min-h-screen object-cover"
-        />
-
         <div className="flex flex-col items-center justify-center h-full w-full px-2 md:px-10">
           <div className="z-1 md:max-w-3xl w-full bg-gray-900/90 filter backdrop-blur-sm py-4 rounded-md px-2 md:px-10 flex flex-col items-center">
             <h1 className="font-chalk uppercase font-bold text-3xl md:text-4xl bg-gradient-to-br  from-brand-white to-brand-green bg-clip-text text-transparent mt-3">
